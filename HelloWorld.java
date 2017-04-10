8a353ef1b15197a3b3c4e921810136f5b9ad6986
lisz1012 <lisz1012@163.com>
2017-04-09 20:31:52 -0700

    Create HelloWorld.java
    The origianl version. Only create a java "Hello world".

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+// This is my first java project with GitHub
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
